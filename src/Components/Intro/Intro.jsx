5d6a0415e9b2929b0e4b959f7787e93150204924
alanacalvo <alana.keeton@gmail.com>
2022-08-08 22:42:22 -0400

    .

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,8 +1,38 @@
 import './Intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(()=> { // running twice at once.
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Engineer', 'Developer', 'Designer']
+    })
+  },[])
+
   return (
-    <div className='intro'>Intro
+    <div className='intro' id='intro'>
+      <div className="left">
+    <div className="imgContainer">
+      <img src='assets/alana.png' alt='portfolio-owner-image'></img>
+    </div>
+      </div>
+      <div className="right">
+    <div className="wrapper">
+      <h2>
+        Hi there!
+      </h2>
+      <h1>I'm Alana Calvo</h1>
+      <h3>Freelance <span ref={textRef}></span></h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque quo corporis, deleniti tempora veniam sapiente quam blanditiis cumque, asperiores rem architecto aspernatur eius delectus quas vel? Reiciendis dolores necessitatibus odio?</p>
+    </div>
+    <a href='#skills'>V</a>
+      </div>
   </div>
   )
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,37 @@ import './Projects.scss'
 function Projects() {
   return (
     <div className='projects' id='projects'>
-      Projects
+      <h1>Projects</h1>
+      <ul>
+        <li className='active'>2048</li>
+        <li>GamerList</li>
+        <li>JellyJam</li>
+        <li>FocusUp</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src='assets/react.png' alt=''/>
+          <h3>2048</h3>
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <img src='assets/react.png' alt=''/>
+          <h3>GamerList</h3>
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <img src='assets/react.png' alt=''/>
+          <h3>JellyJam</h3>
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <img src='assets/react.png' alt=''/>
+          <h3>Focus App</h3>
+        </div>
+      </div>
       </div>
   )
 }
