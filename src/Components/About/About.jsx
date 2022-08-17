b2c07f1cafc69de97ff14d1c0362ba56ffe8ea45
alanacalvo <alana.keeton@gmail.com>
2022-08-17 13:47:55 -0400

    fixed about me

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,11 +8,10 @@ function About() {
           {/* <hr></hr> */}
           <p>Hi, I'm Alana and I enjoy creating functional, beautiful, and efficient applications. It took me a long time to get here though. I could never figure out what it was that I wanted to do in life. I spent my time with my kids, and working various jobs that I enjoyed, and some that I did not. It wasn't until three people in my circle became software engineers, that I decided to check it out and see if it was for me too. Turns out it was, and now here I am, doing something that I absolutely love. I am thrilled to be a part of the ever growing tech field.</p>
             
-          <p>In my free time, you can find me hanging out with family and friends, binge watching movie and shows, or exploring a new city.</p>
+          <p>In my free time, I enjoy doing puzzles, playing video games, trying out new foods and drinks, and reading a good murder mystery novel. My favorite place to be is anywhere near or in the water, hanging with family and friends!</p>
 
-          <p>I am a proud graduate of General Assembly's Sofwate Engineering Immersive Boot Camp. I am open to work, so feel free to reach out with any opportunities!</p>
-        </div>
-
+          <p>I am a proud graduate of General Assembly's Software Engineering Immersive Boot Camp. I am open to work, so feel free to reach out with any opportunities!</p>
+      </div>
     </div>
   )
 }
